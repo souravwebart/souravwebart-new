f73844af06229998b465ba29a85398a500db3223
souravwebart <guptasourav536@gmail.com>
2022-11-29 01:27:42 +0530

    fixed

--- Component/Header.tsx
+++ Component/Header.tsx
@@ -24,7 +24,7 @@ function Header() {
         >
           <Link href="/">
             <Image
-              src="/souravwebart.png"
+              src="/Souravwebart.png"
               alt="souravwebart"
               width={150}
               height={50}
