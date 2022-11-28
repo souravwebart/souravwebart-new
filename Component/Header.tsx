5925fa1d36067cf62bece10cf8d6b749b699a6c0
souravwebart <guptasourav536@gmail.com>
2022-11-29 00:50:13 +0530

    test

--- Component/Header.tsx
+++ Component/Header.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { BsFilterRight, BsXLg } from "react-icons/bs";
 import Link from "next/link";
-import Logo from "../public/Image/souravwebart.png";
 
 function Header() {
   const [show, setShow] = useState(true);
@@ -24,7 +23,12 @@ function Header() {
           }}
         >
           <Link href="/">
-            <Image src={Logo} alt="souravwebart" width={150} height={50} />
+            <Image
+              src={`/souravwebart.png`}
+              alt="souravwebart"
+              width={150}
+              height={50}
+            />
           </Link>
         </div>
         <input type="checkbox" id="active" onClick={openMenu} />
