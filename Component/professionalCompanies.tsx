5b61b7680fcddeabed914a118d4f1ca971012832
souravwebart <guptasourav536@gmail.com>
2024-05-04 01:45:14 +0530

    change some text

--- Component/professionalCompanies.tsx
+++ Component/professionalCompanies.tsx
@@ -1,48 +1,49 @@
-import React from "react";
-import Image from "next/image";
-import Slider from "./SubComponent/slider";
-import CompanyList from "./Data/companyList";
-import { useRouter } from "next/router";
+/** @format */
+
+import React from 'react';
+import Image from 'next/image';
+import Slider from './SubComponent/slider';
+import CompanyList from './Data/companyList';
+import { useRouter } from 'next/router';
 
 function ProfessionalCompanies() {
-  const router = useRouter();
-  return (
-    <div
-      className="professionCompanySection"
-      style={{
-        padding: "80px 0px",
-        textAlign: "center",
-        backgroundColor: router.pathname === "/about" ? "#412678" : "#F2F7FA",
-      }}
-    >
-      <div className="container">
-        <div
-          className="professional_section_heading"
-          style={{ marginBottom: "45px" }}
-        >
-          <h2
-            style={{ color: router.pathname === "/about" ? "#fff" : "#2e216" }}
-          >
-            Professional Experience Companies{" "}
-          </h2>
-          <p
-            style={{
-              color:
-                router.pathname === "/about"
-                  ? "rgb(224 185 244)"
-                  : "rgb(90, 90, 90)",
-            }}
-          >
-            I have worked for below mentioned companies and <br></br> currently
-            working at thumbstack technologies.
-          </p>
-        </div>
-        <div className="professional_section_logo">
-          <Slider CompanyList={CompanyList} />
-        </div>
-      </div>
-    </div>
-  );
+	const router = useRouter();
+	return (
+		<div
+			className='professionCompanySection'
+			style={{
+				padding: '80px 0px',
+				textAlign: 'center',
+				backgroundColor: router.pathname === '/about' ? '#412678' : '#F2F7FA',
+			}}
+		>
+			<div className='container'>
+				<div
+					className='professional_section_heading'
+					style={{ marginBottom: '45px' }}
+				>
+					<h2
+						style={{ color: router.pathname === '/about' ? '#fff' : '#2e216' }}
+					>
+						Professional Experience Companies{' '}
+					</h2>
+					<p
+						style={{
+							color:
+								router.pathname === '/about'
+									? 'rgb(224 185 244)'
+									: 'rgb(90, 90, 90)',
+						}}
+					>
+						I have worked for below mentioned Amaizing companies.
+					</p>
+				</div>
+				<div className='professional_section_logo'>
+					<Slider CompanyList={CompanyList} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProfessionalCompanies;
