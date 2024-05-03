2cd20eadcf09d393e5a88858f66ac07e5a17e976
souravwebart <guptasourav536@gmail.com>
2024-05-03 20:57:09 +0530

    error fixed

--- Component/HomeComponent.tsx
+++ Component/HomeComponent.tsx
@@ -25,19 +25,12 @@ import {
 	ZoomOut,
 } from 'react-scroll-motion';
 import { fadeInLeft } from 'react-animations';
-import Radium, { StyleRoot } from 'radium';
 import Image from 'next/image';
 import Rocket1 from './Image/globe.png';
 import Rocket2 from '../public/Image/sourav gupta (2).png';
 import Link from 'next/link';
 
 function HomeComponent() {
-	const styles = {
-		fadeinleft: {
-			animation: 'x 1s',
-			animationName: Radium.keyframes(fadeInLeft, 'fadeinleft'),
-		},
-	};
 	return (
 		<div
 			style={{ minHeight: '95vh', position: 'relative' }}
@@ -47,42 +40,39 @@ function HomeComponent() {
 				<div className='row'>
 					<div className='col col-12' style={{ margin: 'auto' }}>
 						<div style={{ paddingTop: '25vh' }}>
-							<StyleRoot>
-								<div style={styles.fadeinleft} className='Topheading'>
-									<h1>
-										Shapping Your <br></br>
-										<span className='subtopheading'>digital vission</span>
-									</h1>
-								</div>
-								<div style={{ maxWidth: '570px' }}>
-									<p>
-										<span
-											style={{
-												fontFamily: 'DM Sans',
-												fontSize: '1.2rem',
-												fontWeight: 500,
-												lineHeight: '32px',
-												color: '#fff',
-											}}
-										>
-											Product management and Delivery / Web App Design &
-											Development/ Startup Consultant / Founder / Front-End
-											Development Professional / SEO - Organic growth for
-											Startups
-										</span>
-									</p>
-								</div>
-								<div>
-									<Link href='/about'>
-										<a
-											data-scroll-to='true'
-											className='header__hero--cta Gradiant-heading '
-										>
-											Lets Start to know me
-										</a>
-									</Link>
-								</div>
-							</StyleRoot>
+							<div className='Topheading'>
+								<h1>
+									Shapping Your <br></br>
+									<span className='subtopheading'>digital vission</span>
+								</h1>
+							</div>
+							<div style={{ maxWidth: '570px' }}>
+								<p>
+									<span
+										style={{
+											fontFamily: 'DM Sans',
+											fontSize: '1.2rem',
+											fontWeight: 500,
+											lineHeight: '32px',
+											color: '#fff',
+										}}
+									>
+										Product management and Delivery / Web App Design &
+										Development/ Startup Consultant / Founder / Front-End
+										Development Professional / SEO - Organic growth for Startups
+									</span>
+								</p>
+							</div>
+							<div>
+								<Link href='/about'>
+									<a
+										data-scroll-to='true'
+										className='header__hero--cta Gradiant-heading '
+									>
+										Lets Start to know me
+									</a>
+								</Link>
+							</div>
 						</div>
 					</div>
 				</div>
