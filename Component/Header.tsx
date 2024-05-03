acff9507113e7e48f313acfff7994a1bd3cb5e7c
souravwebart <guptasourav536@gmail.com>
2024-05-03 20:40:25 +0530

    update

--- Component/Header.tsx
+++ Component/Header.tsx
@@ -1,84 +1,90 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import { BsFilterRight, BsXLg } from "react-icons/bs";
-import Link from "next/link";
+/** @format */
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import BG from './Image/bg.png';
+import { BsFilterRight, BsXLg } from 'react-icons/bs';
+import Logo from '../public/Souravwebart.png';
+import Link from 'next/link';
 
 function Header() {
-  const [show, setShow] = useState(true);
+	const [show, setShow] = useState(true);
 
-  const openMenu = () => {
-    setShow(!show);
-  };
+	const openMenu = () => {
+		setShow(!show);
+	};
 
-  return (
-    <div className="main_header">
-      <div className="container">
-        <div
-          style={{
-            top: "20px",
-            left: "20px",
-            position: "absolute",
-            zIndex: 3,
-            cursor: "pointer",
-          }}
-        >
-          <Link href="/">
-            <Image
-              src="/Souravwebart.png"
-              alt="souravwebart"
-              width={150}
-              height={50}
-            />
-          </Link>
-        </div>
-        <input type="checkbox" id="active" onClick={openMenu} />
-        <label htmlFor="active" className="menu-btn">
-          {show ? (
-            <BsFilterRight className="icon-menu" size={"3rem"} />
-          ) : (
-            <BsXLg />
-          )}
-        </label>
-        <div className="wrapper ">
-          <input type="checkbox" id="active" onClick={openMenu} />
-          <label htmlFor="active" className="menu-btn">
-            {show ? (
-              <BsFilterRight className="icon-menu" size={"3rem"} />
-            ) : (
-              <BsXLg />
-            )}
-          </label>
-          <ul>
-            <li>
-              <Link href="/">
-                <a className="Gradiant-heading">Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/about">
-                <a className="Gradiant-heading">About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/project">
-                <a className="Gradiant-heading">Projects</a>
-              </Link>
-            </li>
-            <li>
-              <a className="Gradiant-heading" href="#">
-                Blog <span>Coming Soon</span>
-              </a>
-            </li>
-            <li>
-              <Link href="/contact">
-                <a className="Gradiant-heading">Lets talk!</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='main_header'>
+			<div className='container'>
+				<div
+					className='col col-12'
+					style={{ display: 'flex', justifyContent: 'space-between' }}
+				>
+					<div
+						style={{
+							// top: '20px',
+							// left: '20px',
+							position: 'relative',
+							margin: 'auto 0px',
+							display: 'block',
+							zIndex: 3,
+							cursor: 'pointer',
+							verticalAlign: 'middle',
+							paddingTop: '20px',
+						}}
+					>
+						<Link href='/'>
+							<Image src={Logo} alt='souravwebart' width={150} height={50} />
+						</Link>
+					</div>
+					<div>
+						<input type='checkbox' id='active' onClick={openMenu} />
+						<label htmlFor='active' className='menu-btn1'>
+							{show ? (
+								<BsFilterRight className='icon-menu' size={'3rem'} />
+							) : (
+								false
+							)}
+						</label>
+						<div className='wrapper '>
+							<input type='checkbox' id='active' onClick={openMenu} />
+							<label htmlFor='active' className='menu-btn'>
+								{show ? false : <BsXLg />}
+							</label>
+							<ul>
+								<li>
+									<Link href='/'>
+										<a className='Gradiant-heading'>Home</a>
+									</Link>
+								</li>
+								<li>
+									<Link href='/about'>
+										<a className='Gradiant-heading'>About</a>
+									</Link>
+								</li>
+								<li>
+									<Link href='/project'>
+										<a className='Gradiant-heading'>Projects</a>
+									</Link>
+								</li>
+								<li>
+									<a className='Gradiant-heading' href='#'>
+										Blog <span>Coming Soon</span>
+									</a>
+								</li>
+								<li>
+									<Link href='/contact'>
+										<a className='Gradiant-heading'>Lets talk!</a>
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
--- Component/HomeComponent.tsx
+++ Component/HomeComponent.tsx
@@ -1,57 +1,100 @@
-import React from "react";
+/** @format */
 
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react';
+import dynamic from 'next/dynamic';
+const Animator = dynamic(
+	import('react-scroll-motion').then((it) => it.Animator),
+	{ ssr: false }
+);
+
+import {
+	ScrollContainer,
+	ScrollPage,
+	batch,
+	Fade,
+	FadeIn,
+	FadeOut,
+	Move,
+	MoveIn,
+	MoveOut,
+	Sticky,
+	StickyIn,
+	StickyOut,
+	Zoom,
+	ZoomIn,
+	ZoomOut,
+} from 'react-scroll-motion';
+import { fadeInLeft } from 'react-animations';
+import Radium, { StyleRoot } from 'radium';
+import Image from 'next/image';
+import Rocket1 from './Image/globe.png';
+import Rocket2 from '../public/Image/sourav gupta (2).png';
+import Link from 'next/link';
 
 function HomeComponent() {
-  return (
-    <div
-      style={{ minHeight: "100vh", position: "relative" }}
-      className="hometop"
-    >
-      <div className="container">
-        <div className="row">
-          <div className="col col-12" style={{ margin: "auto" }}>
-            <div style={{ paddingTop: "25vh" }}>
-              <div className="Topheading">
-                <h1>
-                  Goal-oriented professional<br></br> targeting challenging
-                  assignments in <br></br>PROJECT EXECUTION & DELIVERY / WEB
-                  DESIGN &
-                </h1>
-                <br></br>
-                <p>
-                  <span className="Gradiant-heading">
-                    FRONT-END DEVELOPMENT
-                  </span>{" "}
-                  World...
-                </p>
-              </div>
-              <Link href="/about">
-                <a
-                  data-scroll-to="true"
-                  className="header__hero--cta Gradiant-heading "
-                >
-                  Lets Start to know me
-                </a>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        style={{ bottom: 0, right: "20px", position: "absolute" }}
-        className="moving-rocket"
-      >
-        <Image
-          src="/sourav gupta (2).png"
-          alt="souravwebart"
-          width={300}
-          height={200}
-        />
-      </div>
-    </div>
-  );
+	const styles = {
+		fadeinleft: {
+			animation: 'x 1s',
+			animationName: Radium.keyframes(fadeInLeft, 'fadeinleft'),
+		},
+	};
+	return (
+		<div
+			style={{ minHeight: '95vh', position: 'relative' }}
+			className='hometop'
+		>
+			<div className='container'>
+				<div className='row'>
+					<div className='col col-12' style={{ margin: 'auto' }}>
+						<div style={{ paddingTop: '25vh' }}>
+							<StyleRoot>
+								<div style={styles.fadeinleft} className='Topheading'>
+									<h1>
+										Shapping Your <br></br>
+										<span className='subtopheading'>digital vission</span>
+									</h1>
+								</div>
+								<div style={{ maxWidth: '570px' }}>
+									<p>
+										<span
+											style={{
+												fontFamily: 'DM Sans',
+												fontSize: '1.2rem',
+												fontWeight: 500,
+												lineHeight: '32px',
+												color: '#fff',
+											}}
+										>
+											Product management and Delivery / Web App Design &
+											Development/ Startup Consultant / Founder / Front-End
+											Development Professional / SEO - Organic growth for
+											Startups
+										</span>
+									</p>
+								</div>
+								<div>
+									<Link href='/about'>
+										<a
+											data-scroll-to='true'
+											className='header__hero--cta Gradiant-heading '
+										>
+											Lets Start to know me
+										</a>
+									</Link>
+								</div>
+							</StyleRoot>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div
+				style={{ bottom: 0, right: '20px', position: 'absolute' }}
+				className='moving-rocket'
+			>
+				<Image src={Rocket2} alt='souravwebart' width={300} height={200} />
+			</div>
+		</div>
+	);
 }
 
 export default HomeComponent;
